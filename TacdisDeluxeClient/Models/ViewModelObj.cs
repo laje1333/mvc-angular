153e987511828520e69f7323bb4f6b3c6431bf88
gustavgurraeriksson <gustav_eriksson_@hotmail.com>
2016-12-07 13:57:00 +0100

    ViewModelObj

---
+++ TacdisDeluxeClient/Models/ViewModelObj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TacdisDeluxeClient.Models
+{
+    public class ViewModelObj
+    {
+        public string Id { get; set; }
+        public List<MenuHeadLine> MenyData { get; set; }
+    }
+
+    public class MenuHeadLine
+    {
+        public string MenuHeadLineTitel { get; set; }
+        public List<MenuSubLine> MenuSubLine { get; set; }
+    }
+
+    public class MenuSubLine
+    {
+        public string MenuSubLineTitel { get; set; }
+        public string Controller { get; set; }
+        public string ViewUrl { get; set; }
+    }
+}
