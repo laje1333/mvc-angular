d8d614099a515db507345c14a8e1bdd8475a341c
Kalle Henriksson <kalle.henriksson@afconsult.com>
2016-12-07 15:34:53 +0100

    Ajaxanrop från menyn som fyller en div med en partialvy.

---
+++ TacdisDeluxeClient/Scripts/Custom/accordion-menu.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var simpleAccordionMenu = $('.accordion-menu');
+
+    if (simpleAccordionMenu.length) {
+        simpleAccordionMenu.find('a.accordion-menu-main-item-text').on('click', function () {
+            simpleAccordionMenu.find('ul.accordion-sub-menu').hide('fast');
+            $(this).closest('li').children('ul').show('fast');
+        });
+    }
+});
