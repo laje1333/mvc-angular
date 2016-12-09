74fd636b35619ced7a1b3ed2396d9a35a0ae9106
gustavgurraeriksson <gustav_eriksson_@hotmail.com>
2016-12-09 10:52:32 +0100

    webapi

--- TacdisDeluxeClient/Scripts/App/Controllers/Invoice/InvoiceSearchController.js
+++ TacdisDeluxeClient/Scripts/App/Controllers/Invoice/InvoiceSearchController.js
@@ -1,25 +1,29 @@
-﻿invoices = [
-    {
-        "Name": "Alfreds Futterkiste",
-        "Country": "Germany"
-    },
-    {
-        "Name": "Berglunds snabbköp",
-        "Country": "Sweden"
-    },
-    {
-        "Name": "Centro comercial Moctezuma",
-        "Country": "Mexico"
-    },
-    {
-        "Name": "Ernst Handel",
-        "Country": "Austriaaa"
-    }
-];
+﻿//invoices = [
+//    {
+//        "Name": "Alfreds Futterkiste",
+//        "Country": "Germany"
+//    },
+//    {
+//        "Name": "Berglunds snabbköp",
+//        "Country": "Sweden"
+//    },
+//    {
+//        "Name": "Centro comercial Moctezuma",
+//        "Country": "Mexico"
+//    },
+//    {
+//        "Name": "Ernst Handel",
+//        "Country": "Austriaaa"
+//    }
+//];
 
 
-tacdisDeluxeApp.controller("InvoiceSearchCtrl", function ($scope) {
+tacdisDeluxeApp.controller("InvoiceSearchCtrl", function ($scope, $http) {
     $scope.searchInvoices = function () {
-        $scope.records = invoices;
+        $http.get("http://localhost:57661/api/invoice/GetInvoice?query=1")
+    .then(function (response) {
+        $scope.testget = response.data;
+    });
+       // $scope.records = invoices;
     };
 });
