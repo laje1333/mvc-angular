c8670fd558751fe9f2688fa9a46b8bfd05ed5164
jonathanrydholm <jonathanrydholm@outlook.com>
2016-12-09 09:59:55 +0100

    Added mockdata folder in api

---
+++ TacdisDeluxeAPI/Controllers/VehicleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TacdisDeluxeAPI.Controllers
+{
+    public class VehicleController : ApiController
+    {
+        // GET: api/Vehicle
+        [HttpGet]
+        public IEnumerable<string> Get(int id)
+        {
+            switch(id){
+                case 1:
+                    Console.WriteLine("hej");
+                    break;
+            }
+                
+
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Vehicle/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Vehicle
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Vehicle/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Vehicle/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- TacdisDeluxeClient/Scripts/App/Controllers/Vehicle/VehicleController.js
+++ TacdisDeluxeClient/Scripts/App/Controllers/Vehicle/VehicleController.js
@@ -2,30 +2,19 @@
 
 
 tacdisDeluxeApp.controller("VehicleController", function ($scope) {
-    $scope.mechanicsVisible = true;
-    $scope.packagesVisible = false;
-
-
-    $scope.setVisibility = function (tab) {
-        switch (tab) {
-            case "mechanics":
-                $scope.mechanicsVisible = true;
-                $scope.packagesVisible = false;
-                return;
-            case "packages":
-                $scope.mechanicsVisible = false;
-                $scope.packagesVisible = true;
-                return;
-        }
-
-    }
+    
+    
 });
 
 
 tacdisDeluxeApp.config(function ($routeProvider) {
     $routeProvider
-        .when('/buildVehicle', {
-            templateUrl: '/AngularTemplates/Vehicle/BuildVehicle/BuildVehicle.html',
+        .when('/newVehicle', {
+            templateUrl: '/AngularTemplates/Vehicle/Sales/NewVehicle.html',
             controller: 'VehicleController'
-        });
+        })
+        .when('/usedVehicle', {
+        templateUrl: '/AngularTemplates/Vehicle/Sales/UsedVehicle.html',
+        controller: 'VehicleController'
+    });
 });
