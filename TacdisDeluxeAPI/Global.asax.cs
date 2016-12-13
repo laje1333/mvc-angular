c304e3c1d1ff87e44c8d9b31105172ee2d036214
Kalle Henriksson <kalle.henriksson@afconsult.com>
2016-12-13 08:05:26 +0100

    Postfix... hahhahah...

--- TacdisDeluxeAPI/Global.asax.cs
+++ TacdisDeluxeAPI/Global.asax.cs
@@ -18,5 +18,13 @@ namespace TacdisDeluxeAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+            {
+                Response.Flush();
+            }
+        }
     }
 }
