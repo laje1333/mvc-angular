254f449c1d636cbe6c979dc9a374d8d06de89816
jonathanrydholm <jonathanrydholm@outlook.com>
2016-12-13 13:01:03 +0100

    Changed cross origin config location

--- TacdisDeluxeClient/Scripts/App/Directives/Vehicle/NewVehicleDirective.js
+++ TacdisDeluxeClient/Scripts/App/Directives/Vehicle/NewVehicleDirective.js
@@ -14,3 +14,20 @@
     };
 })
 
+tacdisDeluxeApp.directive('panelheaderwithclick', function () {
+    return {
+        restrict: 'AE',
+        template: '<div class="panel-heading">{{title}}</div>',
+
+        scope: {
+            title: '@title'
+        },
+        replace: true,
+        link: function (scope, element, attrs) {
+            $(element).click(function () {
+                $(element).slideToggle();
+            });
+
+        }
+    };
+})
