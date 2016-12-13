f52b9b7d1221f2e32e525fe81397471fed62f464
jonathanrydholm <jonathanrydholm@outlook.com>
2016-12-13 10:15:13 +0100

    Fixade post

---
+++ TacdisDeluxeAPI/Mockdata/VehicleData/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TacdisDeluxeAPI.Mockdata.VehicleData
+{
+    public class Car
+    {
+        public string Name { get; set; }
+    }
+}
