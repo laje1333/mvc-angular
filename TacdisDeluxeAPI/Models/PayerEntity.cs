dbe5bb05d614eb24db2d8a9b6dcd83ed56f74650
Kalle Henriksson <kalle.henriksson@afconsult.com>
2016-12-16 08:51:24 +0100

    Blaha

--- TacdisDeluxeAPI/Models/PayerEntity.cs
+++ TacdisDeluxeAPI/Models/PayerEntity.cs
@@ -12,14 +12,21 @@ namespace TacdisDeluxeAPI.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string FirsName { get; set; }
+
         public string LastName { get; set; }
+
         public bool Trusted { get; set; }
-        [Index(IsUnique = true)]
+
         public string CustomerNumber { get; set; }
+
         public string StreeatAddress { get; set; }
+
         public string ZipCity { get; set; }
+
         public string Country { get; set; }
+
         public ICollection<SaleEntity> Sales { get; internal set; }
     }
 }
