cb2b13c9f4895a867aab214fc70900a99dbeec16
gustavgurraeriksson <gustav_eriksson_@hotmail.com>
2016-12-20 08:07:13 +0100

    mapper fix

--- TacdisDeluxeAPI/App_Start/AutomapperConfig.cs
+++ TacdisDeluxeAPI/App_Start/AutomapperConfig.cs
@@ -17,12 +17,14 @@ namespace TacdisDeluxeAPI
             {
                 cfg.CreateMap<SaleEntity, SalesDto>();
                 cfg.CreateMap<InvoiceEntity, InvoiceDto>();
+                cfg.CreateMap<List<InvoiceEntity>,List<InvoiceDto>>();
                 cfg.CreateMap<List<InvoiceRowEntity>, List<InvoiceRowDto>>();
                 cfg.CreateMap<InvoiceRowEntity, InvoiceRowDto>();
                 cfg.CreateMap<PayerEntity, PayerDto>();
                 cfg.CreateMap<SalesmanEntity, SalesmanDto>();
 
                 cfg.CreateMap<InvoiceDto, InvoiceEntity>();
+                cfg.CreateMap<List<InvoiceDto>,List<InvoiceEntity>>();
                 cfg.CreateMap<List<InvoiceRowDto>, List<InvoiceRowEntity>>();
                 cfg.CreateMap<InvoiceRowDto,InvoiceRowEntity >();
                 cfg.CreateMap<PayerDto, PayerEntity>();
