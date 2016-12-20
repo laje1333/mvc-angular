ca42e92756bfbc67392fbacbfde90bf818e94d9c
laje1333 <jesper-larsson@live.com>
2016-12-20 10:04:49 +0100

    did stuff so that stuff works and stuff

--- TacdisDeluxeAPI/Controllers/SalesmanController.cs
+++ TacdisDeluxeAPI/Controllers/SalesmanController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TacdisDeluxeAPI.DTO;
 using TacdisDeluxeAPI.Models;
 
 namespace TacdisDeluxeAPI.Controllers
@@ -11,23 +13,53 @@ namespace TacdisDeluxeAPI.Controllers
     public class SalesmanController : ApiController
     {
         // GET: api/Sales
-        public IQueryable<SalesmanEntity> GetParts()
+        public IHttpActionResult Get(string FirstName, string LastName, string Company)
         {
-            using(DBContext db = new DBContext())
+            using (DBContext db = new DBContext())
             {
-                return db.Salesmen;
+                IQueryable<SalesmanEntity> allSalesmen;
+
+                if (string.IsNullOrEmpty(FirstName + LastName + Company))
+                {
+                    return Ok(new List<SalesmanEntity>());
+                }
+                else
+                {
+                    allSalesmen = db.Salesmen as IQueryable<SalesmanEntity>;
+                }
+
+                if (!String.IsNullOrEmpty(FirstName))
+                {
+                    allSalesmen = allSalesmen.Where(p => p.FirstName.ToString().Contains(FirstName));
+                }
+
+                if (!String.IsNullOrEmpty(LastName))
+                {
+                    allSalesmen = allSalesmen.Where(p => p.LastName.Contains(LastName));
+                }
+
+                if (!String.IsNullOrEmpty(Company))
+                {
+                    allSalesmen = allSalesmen.Where(p => p.Company.Contains(Company));
+                }
+
+                return Ok(allSalesmen.ToList());
             }
-            
+
         }
+       
 
 
-        public IHttpActionResult PostSale([FromBody]SalesmanEntity request)
+        public IHttpActionResult PostSalesman([FromBody]SalesmanDto request)
         {
+            var enti = Mapper.Map<SalesmanDto, SalesmanEntity>(request);
+
             try
             {
                 using (DBContext db = new DBContext())
                 {
-                    db.Salesmen.Add(request);
+                    //validate and stuff before adding the new salesman
+                    db.Salesmen.Add(enti);
                     db.SaveChanges();
                 }
             }
