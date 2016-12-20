5edebc0efc9b121f2195732eacf425591783df4f
jonathanrydholm <jonathanrydholm@outlook.com>
2016-12-20 11:40:56 +0100

    Stuff

--- TacdisDeluxeClient/Scripts/App/Controllers/Account/AccountController.js
+++ TacdisDeluxeClient/Scripts/App/Controllers/Account/AccountController.js
@@ -1,5 +1,15 @@
 ﻿tacdisDeluxeApp.controller("AccountController", function ($scope, $http, $route) {
 
 
+    $scope.logIn = function(){
+
+        $http({
+            method: 'POST',
+            url: "http://localhost:57661/api/Account/AddCar",
+            data: vehicleData
+        }).success(function () {
+
+        });
+    }
 
 });
