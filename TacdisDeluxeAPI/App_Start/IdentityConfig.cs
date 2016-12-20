29b6f1e8dff82d1da5aad1a4c38262639631be00
Kalle Henriksson <kalle.henriksson@afconsult.com>
2016-12-20 15:39:03 +0100

    Kalle does stuff

--- TacdisDeluxeAPI/App_Start/IdentityConfig.cs
+++ TacdisDeluxeAPI/App_Start/IdentityConfig.cs
@@ -4,42 +4,61 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using TacdisDeluxeAPI.Models;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
 
 namespace TacdisDeluxeAPI
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
 
-    public class ApplicationUserManager : UserManager<ApplicationUser>
+    public class IdentityConfig
     {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
-            : base(store)
+        public void Configuration(IAppBuilder app)
         {
+            app.CreatePerOwinContext(() => new IdentityContext());
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<RoleManager<AppRole>>((options, context) =>
+                new RoleManager<AppRole>(
+                    new RoleStore<AppRole>(context.Get<IdentityContext>())));
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+            });
         }
 
-        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
-        {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
-            // Configure validation logic for usernames
-            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
-            {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
-            };
-            // Configure validation logic for passwords
-            manager.PasswordValidator = new PasswordValidator
-            {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
-            };
-            var dataProtectionProvider = options.DataProtectionProvider;
-            if (dataProtectionProvider != null)
-            {
-                manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
-            }
-            return manager;
-        }
+        //public IdentityConfig(IUserStore<ApplicationUser> store)
+        //    : base(store)
+        //{
+        //}
+
+        //public static IdentityConfig Create(IdentityFactoryOptions<IdentityConfig> options, IOwinContext context)
+        //{
+        //    var manager = new IdentityConfig(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+        //    // Configure validation logic for usernames
+        //    manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+        //    {
+        //        AllowOnlyAlphanumericUserNames = false,
+        //        RequireUniqueEmail = true
+        //    };
+        //    // Configure validation logic for passwords
+        //    manager.PasswordValidator = new PasswordValidator
+        //    {
+        //        RequiredLength = 6,
+        //        RequireNonLetterOrDigit = true,
+        //        RequireDigit = true,
+        //        RequireLowercase = true,
+        //        RequireUppercase = true,
+        //    };
+        //    var dataProtectionProvider = options.DataProtectionProvider;
+        //    if (dataProtectionProvider != null)
+        //    {
+        //        manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+        //    }
+        //    return manager;
+        //}
+
+
     }
 }
