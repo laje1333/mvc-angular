f8f2d5e218055afc114070ab3528efe3f55eee66
gustavgurraeriksson <gustav_eriksson_@hotmail.com>
2017-01-02 15:51:20 +0100

    Byte av datatyper och api för salesdto

---
+++ TacdisDeluxeAPI/Models/Enums/InvoiceState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TacdisDeluxeAPI.Models.Enums
+{
+    public enum InvoiceState
+    {
+        Preliminary = 1,
+        Active = 2,
+        Cancelled = 3
+    }
+}
