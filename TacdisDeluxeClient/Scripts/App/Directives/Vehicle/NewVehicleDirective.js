0cb3e3ac72eb2ff33b0c21b82b85b78bb42c6741
jonathanrydholm <jonathanrydholm@outlook.com>
2017-01-03 07:11:35 +0100

    tframe

--- TacdisDeluxeClient/Scripts/App/Directives/Vehicle/NewVehicleDirective.js
+++ TacdisDeluxeClient/Scripts/App/Directives/Vehicle/NewVehicleDirective.js
@@ -51,33 +51,56 @@ tacdisDeluxeApp.directive("tframe", function () {
         var offX;
         var offY;
 
-
+        var p;
 
         function addListeners() {
             $scope.frameId = $scope.generateFrameId();
             $scope.frameBodyId = $scope.generateFrameId();
+            $scope.frameHeaderId = $scope.generateFrameId();
+            $scope.resizeId = $scope.generateFrameId();
             document.getElementById("frame").id = $scope.frameId;
             document.getElementById("framebody").id = $scope.frameBodyId;
-
+            document.getElementById("frameheader").id = $scope.frameHeaderId;
+            document.getElementById("resize").id = $scope.resizeId;
             frameTracker.push($scope.frameId);
             selectedFrame = $scope.frameId;
 
-            document.getElementById($scope.frameId).addEventListener('mousedown', mouseDown, false);
+            document.getElementById($scope.frameHeaderId).addEventListener('mousedown', mouseDown, false);
             window.addEventListener('mouseup', mouseUp, false);
             
+            var resizer = document.getElementById($scope.resizeId);
+            resizer.addEventListener('mousedown', initDrag, false);
+            resizer.style.width = document.getElementById($scope.frameId).offsetWidth + "px";
+            document.getElementById($scope.resizeId).style.left = tempX + "px";
+            document.getElementById($scope.resizeId).style.top = (tempY + document.getElementById($scope.frameBodyId).offsetHeight + 50) + "px";
+            p = document.getElementById($scope.frameId);
         }
+
+        
+
+
 
         function mouseUp() {
             window.removeEventListener('mousemove', divMove, true);
+            var div = document.getElementById($scope.frameId);
+            if (div.offsetLeft < 0) {
+                div.style.left = "1px";
+            }
+            if (div.offsetTop < 0) {
+                div.style.top = "auto";
+            }
+            
         }
 
         function mouseDown(e) {
+            selectedFrame = $scope.frameId;
             if ($scope.frameIsMinimized == false) {
                 var div = document.getElementById($scope.frameId);
-
+                var header = document.getElementById($scope.frameHeaderId);
                 offY = e.clientY - parseInt(div.offsetTop);
                 offX = e.clientX - parseInt(div.offsetLeft);
-                window.addEventListener('mousemove', divMove, true);
+                    window.addEventListener('mousemove', divMove, true);
+                
             }
         }
 
@@ -87,11 +110,36 @@ tacdisDeluxeApp.directive("tframe", function () {
                 div.style.position = 'absolute';
                 div.style.top = (e.clientY - offY) + 'px';
                 div.style.left = (e.clientX - offX) + 'px';
+                tempX = (e.clientX - offX);
+                tempY = (e.clientY - offY);
             }
         }
 
+        var tempX, tempY;
+        var startX, startY, startWidth, startHeight;
+        function initDrag(e) {
+            startX = e.clientX;
+            startY = e.clientY;
+            startWidth = parseInt(document.defaultView.getComputedStyle(p).width, 10);
+            startHeight = parseInt(document.defaultView.getComputedStyle(p).height, 10);
+            document.documentElement.addEventListener('mousemove', doDrag, false);
+            document.documentElement.addEventListener('mouseup', stopDrag, false);
+        }
 
+        function doDrag(e) {
 
+            p.style.width = (startWidth + e.clientX - startX) + 'px';
+            p.style.height = (startHeight + e.clientY - startY) + 'px';
+            document.getElementById($scope.frameBodyId).style.width = (document.getElementById($scope.frameId).offsetWidth-50) + "px";
+            document.getElementById($scope.frameBodyId).style.height = (document.getElementById($scope.frameId).offsetHeight - 50) + "px";
+            document.getElementById($scope.resizeId).style.width = document.getElementById($scope.frameId).offsetWidth + "px";
+            document.getElementById($scope.resizeId).style.left = tempX + "px";
+            document.getElementById($scope.resizeId).style.top = (tempY + document.getElementById($scope.frameBodyId).offsetHeight + 50) + "px";
+        }
+
+        function stopDrag(e) {
+            document.documentElement.removeEventListener('mousemove', doDrag, false); document.documentElement.removeEventListener('mouseup', stopDrag, false);
+        }
 
 
 
@@ -122,7 +170,7 @@ tacdisDeluxeApp.directive("tframe", function () {
 
         $scope.minimize = function () {
             if ($scope.frameIsMinimized == false) {
-                var widthOffset = 365;
+                var widthOffset = 300;
 
                 for (i = 0; i < minimizedSlots.length; i++) {
                     widthOffset += document.getElementById(minimizedSlots[i]).offsetWidth;
@@ -131,7 +179,7 @@ tacdisDeluxeApp.directive("tframe", function () {
 
                 document.getElementById($scope.frameId).style.position = "fixed";
                 document.getElementById($scope.frameId).style.width = "auto";
-                document.getElementById($scope.frameId).style.top = "908px";
+                document.getElementById($scope.frameId).style.top = (document.getElementById("appcontainer").clientHeight - 75) + "px";
                 document.getElementById($scope.frameId).style.left = widthOffset + "px";
 
                 $scope.frameIsMinimized = true;
