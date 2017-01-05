59aecf07db34159c34ace21efe3e9f80ca96412a
laje1333 <jesper-larsson@live.com>
2017-01-05 10:02:42 +0100

    Vassego Gurra

---
+++ TacdisDeluxeAPI/DTO/IdAndAmountDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TacdisDeluxeAPI.Models
+{
+    public class IdAndAmountDto
+    {
+        int Id { get; set; }
+        int Amount { get; set; }
+    }
+}
