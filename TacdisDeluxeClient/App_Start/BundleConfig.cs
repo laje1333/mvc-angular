fdc2250d504a0e824fca59b4352f77f598b4e90f
jonathanrydholm <jonathanrydholm@outlook.com>
2017-01-12 07:46:06 +0100

    custom html generating without angular

--- TacdisDeluxeClient/App_Start/BundleConfig.cs
+++ TacdisDeluxeClient/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace TacdisDeluxeClient
                         "~/Scripts/App/app.js",
                         "~/Scripts/Custom/accordion-menu.js",
                         "~/Scripts/Custom/Maths.js",
+                        "~/Scripts/Custom/ActionEvents.js",
                         "~/Scripts/angular-ui/ui-bootstrap.js",
                         "~/Scripts/angular-ui/ui-bootstrap-tpls.js"));
 
